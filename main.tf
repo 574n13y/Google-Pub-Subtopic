6eeaf21f46cad4fcce3d52812ce1ad615d765aa8
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-12-29 18:50:41 +0530

    ✨ All Good in main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ variable "topic_name" {
   description = "Customer"
   type        = string
 }
+
 resource "google_pubsub_topic" "mypub" {
   name = "mypubsub-topic"
 
   labels = {
     foo = "bar"
   }
-
   message_retention_duration = "900000s"
 }
 
