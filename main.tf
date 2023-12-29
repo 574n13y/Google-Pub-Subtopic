b61331731f4ec53d295484b622c01df8812df9e9
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-12-29 16:31:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = "key.json"
-  project     = "vivesh"
+  project     = "vivesh-405513"
   region      = "us-central1"    
 
 }
@@ -23,4 +23,26 @@ resource "google_kms_crypto_key" "crypto_key" {
 resource "google_kms_key_ring" "key_ring" {
   name     = "Customer-keyring"
   location = "global"
+}
+resource "google_pubsub_topic" "vivesh" {
+  name = "demo-topic"
+}
+
+resource "google_pubsub_subscription" "my_sub" {
+  name  = "demo-subscription"
+  topic = google_pubsub_topic.example.name
+
+  ack_deadline_seconds = 20
+
+  labels = {
+    foo = "bar"
+  }
+
+  push_config {
+    push_endpoint = "https://example.com/push"
+
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
 }
