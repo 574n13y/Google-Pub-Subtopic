cfbf4e65ca4ee8083d8029dda8e6d85604111771
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-12-29 16:26:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,21 +5,13 @@ provider "google" {
 
 }
 
-module "terraform-google-pubsub-topic" {
-  source = "github.com/mineiros-io/terraform-google-pubsub-topic?ref=v0.1.2"
-
-  name    = "pub-sub-topic-name"
- project     = "vivesh"
-}
-
-
 variable "topic_name" {
   description = "Customer"
   type        = string
 }
 
 resource "google_pubsub_topic" "pubsub_topic" {
-  name = var.topic_name
+  name = "pubsub_topic_test"
   labels = {
     foo = "bar"
   }
