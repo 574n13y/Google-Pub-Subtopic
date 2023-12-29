afa97419e958df6969f65c5f84c43b8379650ab6
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-12-29 16:13:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,25 +2,28 @@ provider "google" {
   credentials = "key.json"
   project     = "vivesh"
   region      = "us-central1"    
+
 }
 
- module "pubsub_topic" {
-  source  = "sysdiglabs/secure-for-cloud/google"
+module "pubsub_topic" {
+ source  = "sysdiglabs/secure-for-cloud/google"
   version = "0.9.11"
- }
+
+}
+
 
 variable "topic_name" {
   description = "The name of the Pub/Sub topic"
   type        = string
-  
 }
 
 resource "google_pubsub_topic" "pubsub_topic" {
-   name = var.topic_name
+  name = var.topic_name
   labels = {
     foo = "bar"
   }
 
   message_retention_duration = "86600s"
+
 }
 
