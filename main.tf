f2cec91b6fac056bad87bf40a4e4bfefe26d1e78
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-12-29 15:02:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "google" {
   credentials = "key.json"
-  project     = "vivesh-405513"
   region      = "us-central1" 
 }
 
 module "pubsub_topic" {
   source = "github.com/mineiros-io/terraform-google-pubsub-topic?ref=v0.1.2"
   name = "pub-sub-topic"
+  project     = "vivesh-405513"
 
 }
 
