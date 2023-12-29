0e2b2012a7314eaac976d8a297e148bd6bb90590
VIVESH <35293085+574n13y@users.noreply.github.com>
2023-12-29 16:29:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,17 @@ variable "topic_name" {
   type        = string
 }
 
-resource "google_pubsub_topic" "pubsub_topic" {
-  name = "pubsub_topic_test"
-  labels = {
-    foo = "bar"
-  }
-
-  message_retention_duration = "86600s"
-
+resource "google_pubsub_topic" "my_pub_sub" {
+  name         = "Customer-topic"
+  kms_key_name = google_kms_crypto_key.crypto_key.id
 }
 
+resource "google_kms_crypto_key" "crypto_key" {
+  name     = "Customer-key"
+  key_ring = google_kms_key_ring.key_ring.id
+}
+
+resource "google_kms_key_ring" "key_ring" {
+  name     = "Customer-keyring"
+  location = "global"
+}
